daa8cc8583abe782409b75b13fb4275c57f3a5c5
Probir Roy <probir.roy.25@hotmail.com>
2020-04-16 19:32:13 +0530

    Develop the security part

--- DatingApp.API/Dtos/UserForRegisterDTO.cs
+++ DatingApp.API/Dtos/UserForRegisterDTO.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDTO
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(25,MinimumLength=4,ErrorMessage="Password length morethen equal 4 character!")]
         public string Password { get; set; }
     }
 }
